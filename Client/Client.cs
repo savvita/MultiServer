ba75ccacddbe310321826d56b675b651e2d210d7
svt13 <svt13@ukr.net>
2022-06-29 20:38:18 +0300

    add gui for client

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,10 +1,7 @@
 ﻿using ConnectLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
@@ -27,32 +24,112 @@ namespace Client
             }
         }
 
-        public void StartChating()
+        public void ConnectToServer()
         {
-            socket.Connect(iPEndPoint);
-
-            Console.WriteLine("Connected to the server");
-
-            do
+            try
             {
-                Console.Write("Msg: ");
-                string msg = Console.ReadLine();
-                Message.SendMessage(socket, msg);
-
-                if(msg.StartsWith(Message.UploadCode))
-                {
-                    Task.Factory.StartNew(() =>
-                    Message.SendFile(socket, msg.Substring(Message.UploadCode.Length).Trim()));
-                }
-
-                Console.WriteLine($"Server: {Message.ReceiveMessage(socket)}");
-
-                if (((socket.Poll(1000, SelectMode.SelectRead) && (socket.Available == 0)) || !socket.Connected))
-                    break;
-
-            } while (true);
-
-            Console.WriteLine("Disconnected");
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(iPEndPoint);
+            }
+            catch
+            {
+                
+            }
         }
+
+        public void Disconnect()
+        {
+            try
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public bool IsConnected
+        {
+            get => socket.Connected;
+        }
+
+        public bool IsSocketAvailable
+        {
+            get
+            {
+                try
+                {
+                    return !((socket.Poll(1000, SelectMode.SelectRead) && (socket.Available == 0)) || !socket.Connected);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public string SendMessage(string message)
+        {
+            string response = String.Empty;
+            try
+            {
+                if (message != String.Empty)
+                {
+                    Message.SendMessage(socket, message);
+                    response = Message.ReceiveMessage(socket);
+                }
+            }
+            catch
+            {
+                response = "Connection lost";
+                Disconnect();
+            }
+
+            return response;
+        }
+
+        public string Upload(string path)
+        {
+            try
+            {
+                Message.SendMessage(socket, Message.UploadCode);
+                Task.Factory.StartNew(() => Message.SendFile(socket, path));
+            }
+            catch
+            {
+            }
+
+            return Message.ReceiveMessage(socket);
+        }
+
+        //public void StartChating()
+        //{
+        //    socket.Connect(iPEndPoint);
+
+        //    Console.WriteLine("Connected to the server");
+
+        //    do
+        //    {
+        //        Console.Write("Msg: ");
+        //        string msg = Console.ReadLine();
+        //        Message.SendMessage(socket, msg);
+
+        //        if(msg.StartsWith(Message.UploadCode))
+        //        {
+        //            Task.Factory.StartNew(() =>
+        //            Message.SendFile(socket, msg.Substring(Message.UploadCode.Length).Trim()));
+        //        }
+
+        //        Console.WriteLine($"Server: {Message.ReceiveMessage(socket)}");
+
+        //        if (((socket.Poll(1000, SelectMode.SelectRead) && (socket.Available == 0)) || !socket.Connected))
+        //            break;
+
+        //    } while (true);
+
+        //    Console.WriteLine("Disconnected");
+        //}
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,20 +1,27 @@
-﻿using System;
+﻿using Client.View;
+using System;
+using System.Windows.Forms;
 
 namespace Client
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            try
-            {
-                Client client = new Client("127.0.0.1", 8008);
-                client.StartChating();
-            }
-            catch
-            {
-                Console.WriteLine("Cannot connect to the server");
-            }
+            //try
+            //{
+            //    Client client = new Client("127.0.0.1", 8008);
+            //    client.StartChating();
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Cannot connect to the server");
+            //}
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ChatForm());
         }
     }
 }
